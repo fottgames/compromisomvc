20820d9c62e6b084cad56b5d3ff857e67657449b
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2017-07-31 13:38:31 -0400

    Se importa SB Admin 2 como Template

--- application/views/plantilla/Footer.php
+++ application/views/plantilla/Footer.php
@@ -2,6 +2,36 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url() ?>assets/template/vendor/jquery/jquery.min.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="<?php echo base_url() ?>assets/template/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Bootstrap Select JavaScript -->
+	<script src="<?php echo base_url() ?>assets/template/vendor/bootstrap-select-1.12.2/dist/js/bootstrap-select.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/template/vendor/bootstrap-select-1.12.2/dist/js/i18n/defaults-es_CL.min.js"></script>
+
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="<?php echo base_url() ?>assets/template/vendor/metisMenu/metisMenu.min.js"></script>
+
+	<!-- Custom Theme JavaScript -->
+	<script src="<?php echo base_url() ?>assets/template/dist/js/sb-admin-2.js"></script>
+
+	<!--Datatables Js-->
+	<script src="<?php echo base_url() ?>assets/template/vendor/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/template/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+	<script src="<?php echo base_url() ?>assets/template/vendor/datatables-responsive/dataTables.responsive.js"></script>
+	<script type="text/javascript">
+ 		// Se inicializan los popover
+ 		$(function () {
+		  $('[data-toggle="popover"]').popover()
+		})
+
+ 	</script>
+
 </body>
 
 </html>
--- application/views/plantilla/Header.php
+++ application/views/plantilla/Header.php
@@ -6,8 +6,37 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php //echo $data['titulo']; ?>  Taller integrado</title>
-	 <!-- Bootstrap -->
-    <link href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">  
+	
+	<title><?php //echo $data['titulo']; ?>  Compromiso Académico</title>
+
+	<!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url() ?>assets/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Select CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/template/vendor/bootstrap-select-1.12.2/dist/css/bootstrap-select.min.css">
+
+    <!-- MetisMenu CSS -->
+    <link href="<?php echo base_url() ?>assets/template/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url() ?>assets/template/dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <link href="<?php echo base_url() ?>assets/css/style.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="<?php echo base_url() ?>assets/template/vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="<?php echo base_url() ?>assets/template/vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?php echo base_url() ?>assets/template/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
